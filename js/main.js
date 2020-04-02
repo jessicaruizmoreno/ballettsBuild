0f0ef0d5c179bf91f472b2ea19c59c73547b525d
Jessica Ruiz Moreno <jessica_ruiz04@hotmail.com>
2020-04-01 23:56:26 -0400

    finished mobile to tablet. now updating responsiveness for desktop

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 (function() {
 	"use strict";
-	console.log("SEAF Fired");
-	
-	//varibales
-	
-	//functions
-	
-	//listeners
+    console.log("SEAF Fired");
+    
+    
+    window.addEventListener("scroll", function(){
+        if(window.scrollY > 2) {
+          document.getElementsByClassName("social-nav")[0].style.visibility = 'hidden';
+          document.getElementsByClassName("navbar")[0].style.top='0';
+        } else {
+            document.getElementsByClassName("social-nav")[0].style.visibility = 'visible';
+            document.getElementsByClassName("navbar")[0].style.top='';
+        }
+
+    });
 	
 })();
