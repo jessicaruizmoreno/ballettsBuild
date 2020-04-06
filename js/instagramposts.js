926e33a97ff5b139fb07414267c41044f43497ae
Jessica Ruiz Moreno <jessica_ruiz04@hotmail.com>
2020-04-05 20:35:42 -0400

    finished up instagram gallery, now fixing desktop text and banner

--- js/instagramposts.js
+++ js/instagramposts.js
@@ -8,10 +8,11 @@
       posts = response.graphql.user.edge_owner_to_timeline_media.edges;
 	  posts_html = '';
 	  url_html='';
-      for(i=0; i < 8; i++){
-        url = posts[i].node.display_url;
-		posts_html += '<div class="col equal-height"><img src="'+url+'"></div>';
-		url_html='<div class="col equal-height instagram-url"><i class="fab fa-instagram"></i><a href="https://www.instagram.com/balletts_bridal?__a=1"><p>View More</p></a></div>';
+	
+	for(i=0; i < 11; i++){
+	url = posts[i].node.display_url;
+		posts_html +='<div class="col-sm-3 equal-height"><a href="'+url+'"><img src="'+url+'" alt width="640" height="800"></a></div>';
+		url_html='<div class="col-sm-3 equal-height instagram-url"><a href="https://www.instagram.com/balletts_bridal"><i class="fab fa-instagram"><p>View More</p></i></a></div>';
 	}
 
 	  $(".posts").html(posts_html+url_html);
